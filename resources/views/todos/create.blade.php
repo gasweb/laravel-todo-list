1f162354d78e30896ea88f1dcc4d6449711d70c7
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-08 18:59:37 +0300

    Fix form

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <h1>Create todo</h1>
 
-    {!! Form::open(['action' => 'TodosController', 'method' => 'POST']) !!}
-        {{ Form::bsText('text') }}
+    {!! Form::open(['action' => 'TodosController@create', 'method' => 'POST']) !!}
+        {{ Form::bsText('first_name', '', []) }}
     {!! Form::close() !!}
 
 @endsection
