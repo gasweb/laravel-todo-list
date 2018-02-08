eb610b57cf84242b790eb0186c23de74ea649f19
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-08 11:31:38 +0300

    Change order by of list item

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -15,7 +15,8 @@ class TodosController extends Controller
      */
     public function index()
     {
-        $todos = Todo::all();
+//        $todos = Todo::all();
+        $todos = Todo::orderBy('created_at', 'desc')->get();
         return view('todos.index')->with('todos', $todos);
     }
 
