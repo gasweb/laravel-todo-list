b0f050189d3d75062b558b42dcf4e238ca27e47d
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-08 11:23:06 +0300

    Adds index template

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/app')
+@section('content')
+    <h1>Todos</h1>
+    @if(count($todos) > 0)
+        @foreach($todos as $todo)
+            <div class="jumbotron">
+                <h3>{{ $todo->text }} <span class="label label-danger">
+                    {{ $todo->due }}
+                </span>
+                </h3>
+
+            </div>
+        @endforeach
+    @endif
+@endsection
