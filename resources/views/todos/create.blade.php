e9e3362282b959412622316aa0aa79e7a2107c08
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-14 14:16:26 +0300

    Fix form action

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <h1>Create todo</h1>
 
-    {!! Form::open(['action' => 'TodosController@create', 'method' => 'POST']) !!}
+    {!! Form::open(['action' => 'TodosController@store', 'method' => 'POST']) !!}
         {{ Form::bsText('text', '', []) }}
         {{ Form::bsTextArea('body', '', []) }}
         {{ Form::bsText('due', '', []) }}
