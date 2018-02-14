416833c5b58a261f39ee9f26def1060207f21cbb
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-14 11:23:37 +0300

    Adds submit, due and body form elements

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -3,7 +3,10 @@
     <h1>Create todo</h1>
 
     {!! Form::open(['action' => 'TodosController@create', 'method' => 'POST']) !!}
-        {{ Form::bsText('first_name', '', []) }}
+        {{ Form::bsText('text', '', []) }}
+        {{ Form::bsTextArea('body', '', []) }}
+        {{ Form::bsText('due', '', []) }}
+        {{ Form::bsTextSubmit('submit', ['class' => 'btn btn-primary']) }}
     {!! Form::close() !!}
 
 @endsection
