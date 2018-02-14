9b7e7092419fb5fb59e9f08018fb79229cc8ba2c
Igor Sinitsyn <web.gas.sin@gmail.com>
2018-02-14 14:32:46 +0300

    Adds hidden component

--- app/Providers/FormServiceProvider.php
+++ app/Providers/FormServiceProvider.php
@@ -14,6 +14,7 @@ class FormServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Form::component('hidden', 'components.form.hidden', ['name', 'value' => null, 'attributes']);
         Form::component('bsText', 'components.form.text', ['name', 'value', 'attributes']);
         Form::component('bsTextArea', 'components.form.textarea', ['name', 'value', 'attributes']);
         Form::component('bsTextSubmit', 'components.form.submit', ['value' => 'Submit', 'attributes']);
